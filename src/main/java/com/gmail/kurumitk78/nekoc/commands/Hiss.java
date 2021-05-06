af642b38b9fcc7a65b5b7b14cc080dc43cb02c70
Kurumi78 <kurumitk78@gmail.com>
2021-05-06 15:27:10 -0500

    Made the hiss command function properly. It was completely messed up before

--- src/main/java/com/gmail/kurumitk78/nekoc/commands/Hiss.java
+++ src/main/java/com/gmail/kurumitk78/nekoc/commands/Hiss.java
@@ -10,21 +10,23 @@ public class Hiss implements CommandExecutor {
         if (!(sender instanceof Player)) {
             return false;
         }
-        if (args.length == 0 || Bukkit.getPlayer(args[0]) == null) {
-            sender.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " Invalid Input");
-            return false;
-        }
+
+        final Player target = Bukkit.getPlayer(args[0]);
         final Player player = ((Player) sender).getPlayer();
         if (NekoC.isNeko(player)) {
-            return false;
+            if (args.length == 0 || Bukkit.getPlayer(args[0]) == null) {
+                sender.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " Invalid Input");
+                return false;
+            }
+            if (NekoC.globalCommands) {
+                Bukkit.broadcastMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + ChatColor.YELLOW + player.getDisplayName() + ChatColor.LIGHT_PURPLE + " is hissing at " + target.getName());
+            } else {
+                player.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " You have hissed at " + ChatColor.YELLOW + target.getDisplayName() + "!");
+                target.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " You hear " + ChatColor.YELLOW + player.getDisplayName() + ChatColor.LIGHT_PURPLE + " hiss at you!");
+            }
         }
-        final Player target = Bukkit.getPlayer(args[0]);
-        if (NekoC.globalCommands) {
-            Bukkit.broadcastMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + ChatColor.YELLOW + player.getDisplayName() + ChatColor.LIGHT_PURPLE + " is hissing at " + target.getName());
-        } else {
-            player.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " You have hissed at " + ChatColor.YELLOW + target.getDisplayName() + "!");
-            target.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " You hear " + ChatColor.YELLOW + player.getDisplayName() + ChatColor.LIGHT_PURPLE + " hiss at you!");
-        }
+        else{player.sendMessage(NekoC.prefix + ChatColor.LIGHT_PURPLE + " Only Neko's can hiss" + "!");}
+
         return true;
     }
 }
