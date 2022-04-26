0a1305599f9cf1746efb1068a3b1e099f02a6ac9
Kurumi78 <kurumitk78@gmail.com>
2022-04-25 22:01:58 -0500

    Removed unnecceccary Import

--- src/main/java/com/gmail/kurumitk78/nekoc/NekoC.java
+++ src/main/java/com/gmail/kurumitk78/nekoc/NekoC.java
@@ -13,7 +13,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 import java.io.File;
-import java.util.ArrayList; //Appears not to be used in file
 import java.util.List;
 
 public final class NekoC extends JavaPlugin {
